07041e28bec7d99dd1c054f910fbd8fbe4c5319c
0200134 <84560987+0200134@users.noreply.github.com>
2024-11-27 16:14:53 +0900

    Add files via upload

---
+++ output_program.s
@@ -0,0 +1,53 @@
+	.cpu arm7tdmi
+	.arch armv4t
+	.fpu softvfp
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 1
+	.eabi_attribute 30, 6
+	.eabi_attribute 34, 0
+	.eabi_attribute 18, 4
+	.file	"temp_code.c"
+	.text
+	.section	.rodata
+	.align	2
+.LC0:
+	.ascii	"Hello world\000"
+	.align	2
+.LC1:
+	.ascii	"Hello Hello\000"
+	.text
+	.align	2
+	.global	main
+	.syntax unified
+	.arm
+	.type	main, %function
+main:
+	@ Function supports interworking.
+	@ args = 0, pretend = 0, frame = 8
+	@ frame_needed = 1, uses_anonymous_args = 0
+	push	{fp, lr}
+	add	fp, sp, #4
+	sub	sp, sp, #8
+	str	r0, [fp, #-8]
+	str	r1, [fp, #-12]
+	ldr	r0, .L3
+	bl	puts
+	ldr	r0, .L3+4
+	bl	puts
+	mov	r3, #0
+	mov	r0, r3
+	sub	sp, fp, #4
+	@ sp needed
+	pop	{fp, lr}
+	bx	lr
+.L4:
+	.align	2
+.L3:
+	.word	.LC0
+	.word	.LC1
+	.size	main, .-main
+	.ident	"GCC: (Arm GNU Toolchain 13.3.Rel1 (Build arm-13.24)) 13.3.1 20240614"
